ae78cfe0a57c3cf8d7be5ccdfa28289cc281cd02
RvVelthuijsen <r.v.velthuijsen@gmail.com>
2023-07-27 14:56:41 +0200

    code cleanup

--- console-only/main.js
+++ console-only/main.js
@@ -1,6 +1,8 @@
+// RUN WITH "NPM RUN START" COMMAND
+
 // IMPORTS
 require("dotenv").config();
-const prompt = require("prompt-sync")({ sigint: true });
+// full api address and api key stored in .env file
 
 // VARIABLES
 let allMazes = [];
@@ -11,38 +13,39 @@ let directionPairs = {
   Right: { counter: "Left", tag: 3 },
   Left: { counter: "Right", tag: 4 },
 };
+// above variable stores the "counter" direction (if we just went up, going down would put us where we were etc.) and the number to tag for each direction
 let move = "Up";
 let inMaze = false;
-// let currentMaze = {
-//   name: "Example Maze",
-//   totalTiles: 17,
-//   potentialReward: 104,
-// };
 let currentMaze = {};
 let currentScoreInHand = 0;
 let currentScoreInBag = 0;
 let totalScore = 0;
-// let tag = 2;
 
 // FUNCTIONS
+
+// Function to fetch all mazes to select maze from
 const fetchAllMazes = async () => {
-  console.log("fetching");
+  console.log("Fetching mazes");
   await fetch(`${process.env.API}/api/mazes/all`, {
     method: "GET",
     headers: { "Authorization": process.env.API_KEY },
   })
     .then((response) => {
-      console.log(response.status);
-      return response.json();
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
     })
     .then((data) => {
+      console.log("Fetching complete. All mazes:");
       allMazes = data;
       console.log(allMazes);
-      console.log("fetching done");
     })
     .catch((error) => console.error(error));
 };
 
+// Function to fetch movementOptions, should data get lost or script be run without first forgetting and registering player
 const fetchLocation = async () => {
   console.log("entering");
   await fetch(`${process.env.API}/api/maze/possibleActions`, {
@@ -52,22 +55,24 @@ const fetchLocation = async () => {
     },
   })
     .then((response) => {
-      console.log(response.status);
-      return response.json();
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
     })
     .then((data) => {
-      console.log(data);
-      location = data;
+      console.log("MoveOptions are:");
+      console.log(data.possibleMoveActions);
     })
     .catch((error) => console.error(error));
 };
 
-const enterTestmaze = async () => {
+// Function to enter Maze, Example maze pre-selected
+const enterMaze = async () => {
   //   currentMaze = allMazes.find((maze) => (maze.name = "Example Maze"));
   currentMaze = allMazes[6];
   totalScore = allMazes[6].potentialReward;
-
-  // set up a way to choose maze (inquirer package)
 
   await fetch(`${process.env.API}/api/mazes/enter?mazeName=Example Maze`, {
     method: "POST",
@@ -76,19 +81,23 @@ const enterTestmaze = async () => {
     },
   })
     .then((response) => {
-      console.log(response.status);
-      return response.json();
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
     })
     .then((data) => {
       console.log(data);
       location = data;
       inMaze = true;
-      console.log("entered");
+      console.log("Entered maze");
     })
     .catch((error) => console.error(error));
 };
 
-const exitTestmaze = async () => {
+//Function to exit Maze
+const exitMaze = async () => {
   console.log("Exiting maze");
   await fetch(`${process.env.API}/api/maze/exit`, {
     method: "POST",
@@ -97,17 +106,19 @@ const exitTestmaze = async () => {
     },
   })
     .then((response) => {
-      console.log(response.status);
-      return response;
-    })
-    .then((data) => {
-      console.log(data);
-      location = data;
-      inMaze = false;
+      if (response.status === 200) {
+        // getting json parse error, so handling like this
+        console.log(response);
+        inMaze = false;
+        console.log("Maze exited");
+      } else {
+        return Promise.reject(response);
+      }
     })
     .catch((error) => console.error(error));
 };
 
+// Function to collect score, resets currentScoreInHand funtion to 0 if succesfull
 const collectScore = async () => {
   console.log("Collecting score");
   await fetch(`${process.env.API}/api/maze/collectScore`, {
@@ -118,18 +129,21 @@ const collectScore = async () => {
   })
     .then((response) => {
       if (response.status === 200) {
-        console.log(`Nice! ${location.currentScoreInHand} added to bag!`);
-        // add multiple lines if score added is 0; all total (add hand and bag together?)
-        currentScoreInHand = 0;
+        return response.json();
+      } else {
+        return Promise.reject(response);
       }
-      return response.json();
     })
     .then((data) => {
-      console.log(data);
+      console.log(
+        `Nice! ${currentScoreInHand} added to bag! Total score in bag is now ${data.currentScoreInBag}`
+      );
+      currentScoreInHand = 0;
     })
     .catch((error) => console.error(error));
 };
 
+// Function to tag tile with direction we go in
 const tagTile = async (tag) => {
   console.log("Tagging tile");
   await fetch(`${process.env.API}/api/maze/tag?tagValue=${tag}`, {
@@ -140,24 +154,18 @@ const tagTile = async (tag) => {
   })
     .then((response) => {
       if (response.status === 200) {
-        console.log(`Tile was tagged ${0}`);
+        return response.json();
+      } else {
+        return Promise.reject(response);
       }
-      return response.json();
     })
     .then((data) => {
-      console.log(data);
+      console.log(`Tile was tagged ${data.tagOnCurrentTile}`);
     })
     .catch((error) => console.error(error));
 };
 
-// const isMovePossible = (lastMove, possibleMove) => {
-//   if (directionPairs[lastMove] === possibleMove) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
-
+// Move selection funtion
 const chooseMove = async () => {
   // if there's only one move, go here
   if (location.possibleMoveActions.length === 1) {
@@ -165,8 +173,9 @@ const chooseMove = async () => {
     move = location.possibleMoveActions[0].direction;
     return;
   }
-  // Creating copy to work with
+  // Creating copy of array to work on
   let moveOptions = Array.from(location.possibleMoveActions);
+  // Creating variables to work with
   let taggedTile;
   let moveSelected = false;
 
@@ -185,7 +194,7 @@ const chooseMove = async () => {
     return;
   }
 
-  // Checking if there's a tag on current tile which corresponds to the direction we went to last time we visited.
+  // Checking if there's a tag on current tile which corresponds to the direction we went to last time we visited and saving it in taggedTile variable
   if (location.tagOnCurrentTile != null) {
     console.log("tag detected");
     taggedTile = moveOptions.find(
@@ -198,41 +207,31 @@ const chooseMove = async () => {
   for (const i of moveOptions) {
     console.log(`Reward is ${i.rewardOnDestination}`);
     if (i.rewardOnDestination > 0) {
-      console.log("we need to go this way");
+      console.log("Score! We need to go this way");
       move = i.direction;
       moveSelected = true;
       break;
     } else if (i.canCollectScoreHere && currentScoreInHand > 0) {
-      console.log("we need to collect score here");
+      console.log("We need to collect score here");
       move = i.direction;
       moveSelected = true;
       break;
     } else if (i.canExitMazeHere && currentScoreInBag === totalScore) {
-      console.log("we need to exit maze here");
+      console.log("We need to exit maze here");
       move = i.direction;
       moveSelected = true;
       break;
     } else if (i.hasBeenVisited === false) {
       console.log(
-        "this is the first tile that has not been visited before, let's go"
+        "This is the first tile that has not been visited before, let's go"
       );
       move = i.direction;
       moveSelected = true;
       break;
     } else if (taggedTile && i.direction === taggedTile.direction) {
-      console.log("we went here before, skipping");
+      console.log("We went here before, skipping");
       continue;
     }
-
-    // else if (move != i.direction) {
-    //   console.log(`last move was ${move}`);
-    //   console.log(`current option is ${i.direction}`);
-    //   console.log(
-    //     "all options visited, going with first option which is not the direction we have been going in"
-    //   );
-    //   move = i.direction;
-    //   break;
-    // }
 
     // make into case
   }
@@ -246,7 +245,6 @@ const chooseMove = async () => {
           location.tagOnCurrentTile != directionPairs[option.direction].tag
       );
     }
-
     console.log(
       "all else failed, going with random option we haven't been before"
     );
@@ -255,14 +253,14 @@ const chooseMove = async () => {
     move = moveOptions[randomOption].direction;
   }
 
+  // tagging tile
   console.log(
     `Going ${move}. Tagging tile for future reference. Next time we come here we should not go here again`
   );
   await tagTile(directionPairs[move].tag);
-
-  // not a catch all... add another break case
 };
 
+// Function to make the api call for movement, awaits move selection (necessary to await because on chooseMove we make an api call to tag)
 const movement = async () => {
   console.log("Starting move");
   await chooseMove();
@@ -274,65 +272,57 @@ const movement = async () => {
     },
   })
     .then((response) => {
-      console.log(response.status);
-      return response.json();
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        return new Promise.reject(response);
+      }
     })
     .then((data) => {
       location = data;
       currentScoreInHand = data.currentScoreInHand;
       currentScoreInBag = data.currentScoreInBag;
-      console.log(data);
     })
     .catch((error) => console.error(error));
 };
 
+// It's easier to follow in console with slight delay, adding timer of 3 sec before making next move
 const timer = (delay) => {
   return new Promise((stop) => setTimeout(stop, delay));
 };
-
 const wait = async () => {
   console.log("Catching breath");
   await timer(3000);
 };
 
+// This is the main gameloop which takes care of the full inital maze setup and loop
 const gameLoop = async () => {
   console.log("start");
   await fetchAllMazes();
-  await enterTestmaze();
+  await enterMaze();
   console.log(currentMaze);
-  console.log(location.possibleMoveActions);
+
   if (!location.possibleMoveActions) {
     await fetchLocation();
   }
   console.log(`In maze is ${inMaze}`);
 
-  // for (let i = 0; i < currentMaze.totalTiles; i++) {
-  //   console.log(`Loop ${i}`);
-  //   // if (location.canCollectScoreHere) {
-  //   //   collectScore();
-  //   // } else if (location.canExitMazeHere) {
-  //   //   exitTestmaze();
-  //   // } else {
-  //   //   await movement();
-  //   // }
-
-  //   await movement();
-  // }
   let i = 1;
   do {
     console.log(`Loop ${i}`);
     if (location.canCollectScoreHere && currentScoreInHand != 0) {
-      collectScore();
+      await collectScore();
     } else if (location.canExitMazeHere && currentScoreInBag === totalScore) {
-      exitTestmaze();
-      console.log("Maze was completed");
+      await exitMaze();
+      console.log("Maze was completed!");
       break;
     }
     await wait();
+    console.log(location.possibleMoveActions);
     await movement();
     i++;
   } while (i < 80);
-  // while doesn't do much if using inMaze (break before is set to false), breaking
+  // while doesn't do much if using inMaze (break before it's set to false), breaking
 };
 
 // EXECUTION
