cfee65881c164d7ee93b240325e07c1933e2e4e8
RvVelthuijsen <r.v.velthuijsen@gmail.com>
2023-07-28 14:47:27 +0200

    basic interactive functionality working

--- console-interactive/main.js
+++ console-interactive/main.js
@@ -11,6 +11,9 @@ let location = {};
 let move = "";
 let currentScoreInHand = 0;
 let currentScoreInBag = 0;
+let totalScore = 0;
+let canExit = false;
+let inMaze = false;
 
 // FUNCTIONS
 const fetchAllMazes = async () => {
@@ -41,6 +44,7 @@ const fetchAllMazes = async () => {
 };
 
 const enterSelectedMaze = async () => {
+  totalScore = selectedMaze.potentialReward;
   await fetch(
     `${process.env.API}/api/mazes/enter?mazeName=${selectedMaze.name}`,
     {
@@ -62,6 +66,7 @@ const enterSelectedMaze = async () => {
     .then((data) => {
       //   console.log(data);
       location = data;
+      inMaze = true;
       console.log(`You have entered the ${selectedMaze.name} maze. Good luck!`);
     })
     .catch((error) => console.error(error));
@@ -77,20 +82,49 @@ const exitMaze = async () => {
   })
     .then((response) => {
       if (response.status === 200) {
-        console.log(response);
         location = {};
         currentScoreInBag = 0;
         currentScoreInHand = 0;
-        //   pathToExit = [];
-        //   canExit = false;
-        //   exitFound = false;
-        //   inMaze = false;
-        //   totalScore = 0;
+        canExit = false;
+        totalScore = 0;
+        inMaze = false;
         console.log("Maze exited");
       } else {
         return Promise.reject(
           `Error: ${response.status} - ${response.statusText}`
         );
+      }
+    })
+    .catch((error) => console.error(error));
+};
+
+const collectScore = async () => {
+  await fetch(`${process.env.API}/api/maze/collectScore`, {
+    method: "POST",
+    headers: {
+      "Authorization": process.env.API_KEY,
+    },
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        return Promise.reject(
+          `Error: ${response.status} - ${response.statusText}`
+        );
+      }
+    })
+    .then((data) => {
+      console.log(
+        `Nice! ${currentScoreInHand} added to your bag! Your total score in bag is now ${data.currentScoreInBag}`
+      );
+      currentScoreInHand = 0;
+      currentScoreInBag = data.currentScoreInBag;
+      if (currentScoreInBag === totalScore) {
+        console.log(
+          "You've added all available score to our bag, so you are now able to exit!"
+        );
+        canExit = true;
       }
     })
     .catch((error) => console.error(error));
@@ -140,15 +174,38 @@ const intitialise = async () => {
   return choices;
 };
 const chooseMove = async () => {
-  const movementOptions = location.possibleMoveActions.map(
-    (option) => option.direction
-  );
+  const movementOptions = location.possibleMoveActions.map((option) => {
+    // switch (true) {
+    //   case option.allowsExit:
+    //     console.log(`You can go ${option.direction} Here you can exit if you've collected all available score.`);
+    //   case option.allowsScoreCollection:
+    //     console.log(`${option.direction} allows you to collect your score.`);
+    //   case option.rewardOnDestination > 0:
+    //     console.log(
+    //       `${option.direction} has a reward: ${option.rewardOnDestination}.`
+    //     );
+    //   default:
+    //     console.log(`${option.direction} has nothing special.`);
+    // }
+
+    console.log(
+      `You can go ${option.direction}. Here you ${
+        option.allowsExit ? "can" : "cannot"
+      } exit, you ${
+        option.allowsScoreCollection ? "can" : "cannot"
+      } cache in your score, and you can collect ${
+        option.rewardOnDestination
+      } score.`
+    );
+
+    return option.direction;
+  });
   const choices = await inquirer
     .prompt([
       {
         type: "list",
         name: "move",
-        message: "Which direction would you like to go in?",
+        message: "Which way would you like to go in?",
         choices: movementOptions,
       },
     ])
@@ -187,17 +244,48 @@ const movement = async () => {
       location = data;
       currentScoreInHand = data.currentScoreInHand;
       currentScoreInBag = data.currentScoreInBag;
-      console.log(
-        `Went ${move}. This is your current status: ${currentScoreInHand} in hand and ${currentScoreInBag} in bag.`
-      );
+      if (currentScoreInBag === totalScore) {
+        canExit = true;
+      }
     })
     .catch((error) => console.error(error));
 
-  if (location.canExitMazeHere) {
-    exitMaze();
-  } else {
-    movement();
+  console.log(
+    `You went ${move}. You are now holding ${currentScoreInHand} score in hand and ${currentScoreInBag} score in bag. The total score here is ${totalScore}`
+  );
+
+  if (location.canExitMazeHere && canExit) {
+    console.log("You have all the available point and you can exit here.");
+    await exitMaze();
+    return;
+  } else if (location.canCollectScoreHere && currentScoreInHand > 0) {
+    const choices = await inquirer
+      .prompt([
+        {
+          type: "confirm",
+          name: "shouldCollectScore",
+          message:
+            "You can collect your score in hand here. Would you like to?",
+          default: false,
+          transformer: (answer) => (answer ? "👍" : "👎"),
+        },
+      ])
+      .catch((error) => {
+        if (error.isTtyError) {
+          console.error(
+            "Prompt couldn't be rendered in the current environment"
+          );
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          console.error(error);
+          // Something else went wrong
+        }
+      });
+    if (choices.shouldCollectScore) {
+      await collectScore();
+    }
   }
+  movement();
 };
 
 const gameLoop = async () => {
@@ -206,25 +294,41 @@ const gameLoop = async () => {
   await intitialise();
   await registerPlayer(playerName);
   await enterSelectedMaze(selectedMaze.name);
-  console.log("You find yourself in a ");
+  console.log(
+    "You find yourself in a new maze. Take a look around, what are your options?"
+  );
   await movement();
 
-  // let i = 1;
-  // do {
-  //   console.log(`Loop ${i}`);
-  //   if (location.canCollectScoreHere && currentScoreInHand != 0) {
-  //     collectScore();
-  //   } else if (location.canExitMazeHere && currentScoreInBag === 94) {
-  //     exitTestmaze();
-  //     console.log("Maze was completed");
-  //     break;
-  //   }
-  //   await wait();
-  //   await movement();
-  //   i++;
-  // } while (i < 80);
+  if (!inMaze) {
+    console.log("Nice you've completed a maze!");
 
-  // while doesn't do much if using inMaze (break before is set to false), breaking
+    const reset = await inquirer
+      .prompt([
+        {
+          type: "confirm",
+          name: "continue",
+          message: "Would you like to play another one?",
+          default: false,
+          transformer: (answer) => (answer ? "👍" : "👎"),
+        },
+      ])
+      .catch((error) => {
+        if (error.isTtyError) {
+          console.error(
+            "Prompt couldn't be rendered in the current environment"
+          );
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          console.error(error);
+          // Something else went wrong
+        }
+      });
+    if (reset.continue) {
+      await intitialise();
+    } else {
+      console.log(`Okay, thank you for playing ${playerName}`);
+    }
+  }
 };
 
 // EXECUTION
